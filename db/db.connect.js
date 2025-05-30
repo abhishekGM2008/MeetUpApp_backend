700be94c1f13a320870fc94697faad1accc89959
Abhishek <abhishek.gmbuj07@gmail.com>
2025-05-30 12:42:08 +0530

    Update db.connect.js

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose")
 require ("dotenv").config()
-const mongoUri = process.env.MONGODB 
 
+const mongoUri = process.env.MONGODB 
 const initialData = async() => {
     await mongoose
     .connect (mongoUri)
-    .then(() => 
-    {
-        console.log("Connected to DataBase")
-    })
+    .then(() => {console.log("Connected to DataBase")})
     .catch((error) => console.log("Error Occured" , error))
 }
-
 module.exports = {initialData}
